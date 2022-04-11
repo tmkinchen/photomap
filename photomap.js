5ca92fe69b6461d8fc61ef70c90191433c169454
tmkinchen <97756277+tmkinchen@users.noreply.github.com>
2022-04-11 15:16:58 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -23,7 +23,7 @@ function createPhotoMap () {
  var OpenStreetMap_France = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 	maxZoom: 20,
 	attribution: '&copy; OpenStreetMap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}.addTo(map);
+}).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
   Papa.parse(csvUrl, {
